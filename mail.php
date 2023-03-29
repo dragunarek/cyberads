8386629fd639287adc731e7b9f2c743ea9441800
Arek Dragun <dragunarek14@gmail.com>
2023-03-29 11:35:40 +0200

    v.3

--- mail.php
+++ mail.php
@@ -1,55 +1,30 @@
 <?php
-    require '/vendor/autoload.php';
-    
-// sprawdzenie, czy formularz został wysłany
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    // pobranie danych z formularza
-    $name = test_input($_POST["name"]);
-    $email = test_input($_POST["email"]);
-    $message = test_input($_POST["message"]);
-    
-    // sprawdzenie, czy pola zostały wypełnione
-    if (empty($name) || empty($email) || empty($message)) {
-        echo "Proszę wypełnić wszystkie pola.";
-        exit;
-    }
-    
-    // sprawdzenie, czy adres e-mail jest poprawny
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Nieprawidłowy adres e-mail.";
-        exit;
-    }
-    
-    // sprawdzenie, czy pole z kodem antyspamowym jest puste
-    if (!empty($_POST["findme"])) {
-        echo "Wysłanie formularza nie powiodło się.";
-        exit;
-    }
-    
-    // ustawienie odbiorcy, tematu i treści e-maila
-    $to = "admin@cyberads.pl";
-    $subject = "Wiadomość ze strony internetowej";
-    $body = "Od: $name\n E-mail: $email\n Wiadomość:\n $message";
-    
-    // ustawienie nagłówków e-maila
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    
-    // wysłanie e-maila
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Wiadomość została wysłana.";
-        header( 'Location: https://cyberads.pl' ); 
-    } else {
-        echo "Wysłanie wiadomości nie powiodło się.";
-    }
-    
-    // funkcja do testowania danych z formularza
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+// Adres e-mail odbiorcy
+$to = "adres_odbiorcy@example.com";
+
+// Temat e-maila
+$subject = "Wiadomość z formularza kontaktowego";
+
+// Zawartość e-maila
+$message = "Imię i nazwisko: " . $_POST['name'] . "\r\n";
+$message .= "E-mail: " . $_POST['email'] . "\r\n";
+$message .= "Wiadomość: " . $_POST['message'];
+
+// Adres e-mail nadawcy
+$from = $_POST['email'];
+
+// Nagłówki e-maila
+$headers = "From:" . $from . "\r\n";
+$headers .= "Reply-To:" . $from . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
+// Wysłanie e-maila
+if(mail($to, $subject, $message, $headers)) {
+    // Wyczyszczenie formularza
+    $_POST = array();
+    // Przekierowanie na stronę główną
+    header("Location: index.html?success=true");
+} else {
+    echo "Wystąpił błąd podczas wysyłania wiadomości.";
 }
 ?>
